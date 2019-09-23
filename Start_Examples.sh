2f8e5aa6b1cd8fa713952a709eec100f60d8a687
char00les <char00les@gmail.com>
2019-09-23 12:33:25 -0400

    Create Start_Examples.sh

---
+++ Start_Examples.sh
@@ -0,0 +1,7 @@
+fmt -u format.txt
+nl combined.txt > numbered.and.combined.txt
+cat delimited.txt
+cut -d ";" -f1 delemited.txt
+cut -d ";" -f2 delemited.txt
+cut -d "]" -f2 delemited.txt
+cat combined.txt >> directorycontents.txt
